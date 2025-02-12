41089770c384a511c9dacfc4bded18ec5ae17f80
GexterJamesLaxamana <gexterlaxamana2@gmail.com>
2025-02-12 20:23:06 +0800

    Update element.sh

--- element.sh
+++ element.sh
@@ -3,7 +3,6 @@
 PSQL="psql --username=freecodecamp --dbname=periodic_table --no-align --tuples-only -c"
 
 if [[ $1 ]]; then
-  # If input is a number, search by atomic_number
   if [[ $1 =~ ^[0-9]+$ ]]; then
     ELEMENT=$($PSQL "SELECT atomic_number, symbol, name, type, atomic_mass, melting_point_celsius, boiling_point_celsius 
                      FROM elements 
@@ -11,7 +10,6 @@ if [[ $1 ]]; then
                      JOIN types USING(type_id) 
                      WHERE atomic_number = $1")
   else
-    # If input is a string, check by symbol or name
     ELEMENT=$($PSQL "SELECT atomic_number, symbol, name, type, atomic_mass, melting_point_celsius, boiling_point_celsius 
                      FROM elements 
                      JOIN properties USING(atomic_number) 
@@ -19,11 +17,9 @@ if [[ $1 ]]; then
                      WHERE symbol = '$1' OR name = '$1'")
   fi
 
-  # If element is not found
   if [[ -z $ELEMENT ]]; then
     echo "I could not find that element in the database."
   else
-    # Read and format output
    echo "$ELEMENT" | while IFS="|" read ATOMIC_NUMBER SYMBOL NAME TYPE MASS MELTING BOILING;
 
     do
